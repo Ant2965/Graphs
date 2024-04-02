20b834ddfe4a001fa5c04a7a21ee29c02b0ca44a
Ant2965 <antrikshbahri@gmail.com>
2024-04-02 16:05:46 +0530

    Starting with Graphs

---
+++ Create.java
@@ -0,0 +1,96 @@
+package gr;
+import java.util.*;
+
+public class Create {
+	static class edge{
+		int source;
+		int destination;
+		int weight;
+		
+		edge(int s,int d,int w){
+			this.source=s;
+			this.destination=d;
+			this.weight=w;
+		}
+	}
+	
+	public static void grcr() {
+
+		int vertice =5;
+		
+		ArrayList<edge> gr[]=new ArrayList[vertice];
+		
+		for(int i=0;i<gr.length;i++) {
+			gr[i]=new ArrayList<>();
+		}
+		
+		
+		
+		
+		
+		gr[0].add(new edge(0,1,5));
+		
+		gr[1].add(new edge(1,0,5));
+		gr[1].add(new edge(1,2,1));
+		gr[1].add(new edge(1,3,3));
+
+		gr[2].add(new edge(2,1,1));
+		gr[2].add(new edge(2,3,1));
+		gr[2].add(new edge(2,4,2));
+		
+		
+		
+		gr[3].add(new edge(3,1,3));
+		gr[3].add(new edge(3,2,1));
+		
+		gr[4].add(new edge(4,2,2));
+		
+		for(int i=0;i<gr[2].size();i++) {
+			
+			edge e= gr[2].get(i);
+			
+			System.out.print(e.destination);
+			
+		}
+		
+		bfs(gr);
+		
+	}
+	public static void bfs(ArrayList<edge>gr[]) {
+		Queue<Integer>q=new LinkedList<>();
+		
+		boolean vis[]=new boolean[gr.length];	
+
+		q.add(0);
+		
+		while(!q.isEmpty()) {
+			
+			int curr=q.poll();
+			if(!vis[curr]) {
+			System.out.println(curr);
+			vis[curr]=true;
+
+			}
+			
+			
+			for(int i=0;i<gr[curr].size();i++) {
+				 int a = gr[curr].get(i).destination;
+				 
+				 q.add(a);
+			}
+		
+		}
+	
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		grcr();
+		
+		
+
+
+	}
+
+}
